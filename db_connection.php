c681f4f7f6ac6fbf329aae13fed79f7476279e6a
NorbertLekawa <norbolek.lekawa@gmail.com>
2025-05-27 14:19:24 +0200

    .

--- db_connection.php
+++ db_connection.php
@@ -3,7 +3,7 @@ session_start();
 $DATABASE_HOST = "localhost";
 $DATABASE_USER = "admin1";
 $DATABASE_PASS = "admin1";
-$DATABASE_NAME = "akontakt";
+$DATABASE_NAME = "e3kontakt";
 $conn=mysqli_connect($DATABASE_HOST,$DATABASE_USER,$DATABASE_PASS,$DATABASE_NAME);
 
 if (mysqli_connect_errno()){
